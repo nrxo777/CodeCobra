59a88d3ffeeee6c1c77b4b6c7d1a47893412bb40
nrxo777 <noorxcalma@gmail.com>
2025-04-21 22:54:56 +0600

    solution_#5

---
+++ #5_weather_activity.py
@@ -0,0 +1,10 @@
+weather = input("What's your weather?! ")
+
+if weather == "sunny":
+    activity = "Go chill out.."
+elif weather == "rainy":
+    activity = "Coffee timmeeee"
+elif weather == "snowy":
+    activity = "Build chopper"
+
+print(activity)
