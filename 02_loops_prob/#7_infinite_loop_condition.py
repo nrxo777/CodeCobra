2570cc29d170470fd2c6e711945ba947536ad607
nrxo777 <noorxcalma@gmail.com>
2025-04-25 00:26:26 +0600

    loop_solution_#7

---
+++ 02_loops_prob/#7_infinite_loop_condition.py
@@ -0,0 +1,8 @@
+
+while True:
+    number = int(input("Enter a value between 1 and 10: "))
+    if 1 <= number <= 10:
+        print("Yoyoyoyyo, good job..")
+        break
+    else:
+        print("arererereeeh, maja nahi aya, pirse try karooww..")
