1cfd7b2dab7bca0ba0a4ff90166029cd0af17501
nrxo777 <noorxcalma@gmail.com>
2025-06-10 17:13:34 +0600

    Add AnimeLength class to extend Anime with episode count functionality

--- 04_OOP/#1_object_class.py
+++ 04_OOP/#1_object_class.py
@@ -6,8 +6,24 @@ class Anime:
     def animeXactor(self):
         return self.anime + " | " + self.actor
 
-anime_list = Anime("HxH", "Gon")
-print(anime_list.anime)
-print(anime_list.actor)
 
-print(anime_list.animeXactor())
+class AnimeLength(Anime):
+    def __init__(self, anime_name, lead_actor, episodes):
+        super().__init__(anime_name, lead_actor)
+        self.eps = "Episodes: " + str(episodes)
+    
+    def animeXactorXeps(self):
+        return self.anime + " | " + self.actor + " | " + self.eps
+
+# anime_list = Anime("HxH", "Gon")
+# print(anime_list.anime)
+# print(anime_list.actor)
+
+# print(anime_list.animeXactor())
+
+anime_length = AnimeLength("Naruto", "Naruto Uzumaki", 720)
+print(anime_length.anime)
+print(anime_length.actor)
+print(anime_length.eps)
+
+print(anime_length.animeXactorXeps())
