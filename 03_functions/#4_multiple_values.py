dbf031b895bd76ff74b612a232ec47aa98a25ba0
nrxo777 <noorxcalma@gmail.com>
2025-06-10 13:15:42 +0600

    Refactor circle function to improve readability and ensure proper rounding of area and circumference

--- 03_functions/#4_multiple_values.py
+++ 03_functions/#4_multiple_values.py
@@ -1,10 +1,12 @@
 import math
 
 def circle(r):
-    _area = math.pi * r ** 2
-    _circum = 2 * math.pi * r
+    _area = (math.pi * r ** 2)
+    area = round(_area, 2)
+    _circum = (2 * math.pi * r)
+    circum = round(_circum, 2)
     
-    return _area, _circum
+    return area, circum
 
 a, c = circle(3)
 print("Area: ", a, "Circumference: ", c)
